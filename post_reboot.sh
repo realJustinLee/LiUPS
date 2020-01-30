78cbedaf077504e8d6bac36405cc8300e3c5c3cd
李欣 <JustinDellAdam@live.com>
2020-01-30 16:12:43 +0800

    Update post_reboot.sh

--- post_reboot.sh
+++ post_reboot.sh
@@ -1,4 +1,6 @@
 # Invoke app via docker-compose
 echo 'Initializing docker-compose'
-docker-compose up -d >> docker-liups.log
+docker-compose build --no-cache
+docker-compose down
+docker-compose up -d >> docker-v2.log
 echo 'Initialized docker-compose'
