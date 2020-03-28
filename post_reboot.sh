2c00cf4858fe33ff821a938bbef8b3693ee27cfa
李欣 <JustinDellAdam@live.com>
2020-03-28 15:41:17 +0800

    Update post_reboot.sh

--- post_reboot.sh
+++ post_reboot.sh
@@ -3,4 +3,8 @@ echo 'Initializing docker-compose'
 docker-compose build --no-cache
 docker-compose down
 docker-compose up -d >> docker-liups.log
-echo 'Initialized docker-compose'
+
+echo 'Auto cleaning...'
+docker system prune -a
+
+echo 'You are all done! Enjoy!'
